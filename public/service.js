28b2fa98810339ed712f29c4c10d454d4a04855f
Eduart2021 <zeedymarina@yahoo.com>
2025-03-02 17:49:11 -0600

    two

--- public/service.js
+++ public/service.js
@@ -1,3 +1,48 @@
 document.getElementById('getStartedButton').addEventListener('click', function() {
     window.location.href = '../public/contactUs.html';
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const yearSpan = document.getElementById('year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+  
+    menuToggle.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent the click from bubbling up
+      navMenu.classList.toggle('active');
+    });
+  
+    document.addEventListener('click', function(event) {
+      if (!navMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+        navMenu.classList.remove('active');
+      }
+    });
+  
+    navMenu.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent clicks inside the menu from closing it
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('backToTop');
+  
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 300) {
+        backToTopButton.style.display = 'block';
+      } else {
+        backToTopButton.style.display = 'none';
+      }
+    });
+  
+    backToTopButton.addEventListener('click', function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
